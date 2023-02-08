6d147b47ce75c2cd1d2777ba1b2c7d628bfb92bd
Luis Eduardo <luiseduardo@MacBook-Pro-de-Luis-2.local>
2023-02-07 22:06:29 -0300

    setup project

---
+++ src/main/kotlin/ExerciseFourteen.kt
@@ -0,0 +1,50 @@
+/*
+ *
+ *  * Copyright 2023  TELOS EDUCACAO CONECTADA LTDA
+ *  *
+ *  *   Licensed under the Apache License, Version 2.0 (the "License");
+ *  *   you may not use this file except in compliance with the License.
+ *  *   You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *   Unless required by applicable law or agreed to in writing, software
+ *  *   distributed under the License is distributed on an "AS IS" BASIS,
+ *  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *   See the License for the specific language governing permissions and
+ *  *   limitations under the License.
+ *
+ */
+
+/**
+ * Exercicio 14
+ *
+ * Escreva um programa que leia três números inteiros e determine se a soma dos dois primeiros é igual ao terceiro.
+ *
+ * @see main onde vamos executar nosso exercicio.
+ *
+ */
+fun main() {
+
+    // EXECUTE AQUI MÉTODO DE LOGICA DO EXERCICIO
+
+}
+
+/**
+ * Essa classe contém método que irá contém a logica do exercicio.
+ */
+class ExerciseFourteen {
+
+    /**
+     * Esse método contém a logica do exercicio
+     *
+     * @param valueOne valor inteiro.
+     * @param valueTwo valor inteiro.
+     * @param valueThree valor inteiro.
+     * @return retorna valor string com resultado do desafio.
+     */
+    fun logicExerciseFourteen(valueOne: Int, valueTwo: Int, valueThree: Int): String {
+        // LOGICA DO EXERCICIO
+        return ""
+    }
+}
